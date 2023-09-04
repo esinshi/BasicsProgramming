600d339f20a68af594295447c328af3b9b8421d7
esinb <esin.bahar00@gmail.com>
2023-09-04 11:43:09 +0200

    exercise 2

--- lec1_ex2_functions.py
+++ lec1_ex2_functions.py
@@ -10,3 +10,16 @@ List integers 1,2,3
 
 '''
 
+letters = ['a', 'b', 'c']
+numbers = [1, 2, 3]
+
+
+def func1(list_letter, list_number):
+    newList = []
+    for i in range(len(list_letter)):
+        newList.append(list_letter[i])
+        newList.append(list_number[i])
+    return newList
+
+
+print(func1(letters, numbers))
